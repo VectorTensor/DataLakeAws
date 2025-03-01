eec2d9690cf757290bc166ea409519fc19f4ece9
Prayash Thapa <comppro54@gmail.com>
2025-03-01 21:57:20 +0545

    feat: add infra for athena query

---
+++ AWSInfra/etl.tf
@@ -0,0 +1,104 @@
+
+# resource "aws_s3_bucket" "bronze" {
+#   bucket = "landing-zone-p64"
+#
+#   force_destroy = true
+#   tags = {
+#     Owner = var.owner_name
+#     Environment = "Dev"
+#   }
+# }
+#
+# resource "aws_s3_bucket" "silver" {
+#   bucket = "cleaned-zone-p64"
+#   force_destroy = true
+#   tags = {
+#     Owner = var.owner_name
+#     Environment = "Dev"
+#   }
+# }
+resource "aws_s3_bucket" "gold" {
+  bucket = "curated-zone-p64"
+  force_destroy = true
+  tags = {
+    Owner = var.owner_name
+    Environment = "Dev"
+  }
+}
+
+# IAM Role for Lambda
+# resource "aws_iam_role" "lambda_etl_role" {
+#   name = "LambdaETLPermissions"
+#
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Principal = {
+#           Service = "lambda.amazonaws.com"
+#         }
+#         Action = "sts:AssumeRole"
+#       }
+#     ]
+#   })
+#
+#   tags = {
+#     Name = "LambdaETLPermissions"
+#   }
+# }
+#
+# resource "aws_iam_role_policy_attachment" "etl_lambda" {
+#
+#   role = aws_iam_role.lambda_etl_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+#
+# }
+#
+# resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+#   role       = aws_iam_role.lambda_etl_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+# }
+#
+# # Create Lambda function
+# resource "aws_lambda_function" "etl_lambda" {
+#   function_name    = "etl_lambda"
+#   runtime         = "python3.9"  # Change runtime if needed
+#   role            = aws_iam_role.lambda_etl_role.arn
+#   handler         = "hello.lambda_handler"
+#   timeout         = 30
+#
+#   # Path to the ZIP package of the Lambda function
+#   filename         = "lambda_function.zip"
+#   source_code_hash = filebase64sha256("lambda_function.zip")
+#   environment  {
+#     variables = {
+#
+#       DEST_BUCKET = aws_s3_bucket.silver.bucket
+#     }
+#   }
+#   tags = {
+#     Name = "ETL Lambda Function"
+#   }
+# }
+#
+# # Optional: Add S3 trigger for Lambda
+# resource "aws_lambda_permission" "s3_trigger" {
+#   statement_id  = "AllowS3Invoke"
+#   action        = "lambda:InvokeFunction"
+#   function_name = aws_lambda_function.etl_lambda.function_name
+#   principal     = "s3.amazonaws.com"
+#   source_arn    = aws_s3_bucket.bronze.arn
+# }
+#
+# resource "aws_s3_bucket_notification" "s3_lambda_trigger" {
+#   bucket = aws_s3_bucket.bronze.id
+#
+#   lambda_function {
+#     lambda_function_arn = aws_lambda_function.etl_lambda.arn
+#     events             = ["s3:ObjectCreated:*"]
+#   }
+#
+#   depends_on = [aws_lambda_permission.s3_trigger]
+# }
+
--- AWSInfra/main.tf
+++ AWSInfra/main.tf
@@ -13,9 +13,8 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_s3_bucket" "bronze" {
-  bucket = "landing-zone-p64"
-
+resource "aws_s3_bucket" "athena_results" {
+  bucket = "my-athena-query-results-p64"
   force_destroy = true
   tags = {
     Owner = var.owner_name
@@ -23,116 +22,89 @@ resource "aws_s3_bucket" "bronze" {
   }
 }
 
-resource "aws_s3_bucket" "silver" {
-  bucket = "cleaned-zone-p64"
-  force_destroy = true
-  tags = {
-    Owner = var.owner_name
-    Environment = "Dev"
-  }
+
+resource "aws_iam_role" "athena_role" {
+  name = "athena-query-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "glue.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
 }
-resource "aws_s3_bucket" "gold" {
-  bucket = "curated-zone-p64"
-  force_destroy = true
-  tags = {
-    Owner = var.owner_name
-    Environment = "Dev"
-  }
-}
-
-# data "aws_ami" "ubuntu_ami" {
-#
-#   most_recent = true
-#   owners = ["amazon"]
-#   filter {
-#     name = "name"
-#     values = ["ubuntu*"]
-#   }
-#
-# }
-#
-# resource "aws_instance" "dev" {
-#   ami = data.aws_ami.ubuntu_ami.id
-#   instance_type = "t2.micro"
-#   subnet_id = "subnet-0f7da863d6389517c"
-#   tags = {
-#     Owner = var.owner_name
-#   }
-# }
-# IAM Role for Lambda
-resource "aws_iam_role" "lambda_etl_role" {
-  name = "LambdaETLPermissions"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-
-  tags = {
-    Name = "LambdaETLPermissions"
-  }
+EOF
 }
 
-resource "aws_iam_role_policy_attachment" "etl_lambda" {
+resource "aws_iam_policy" "athena_s3_access" {
+  name        = "AthenaS3AccessPolicy"
+  description = "Allows Athena to read from S3"
 
-  role = aws_iam_role.lambda_etl_role.name
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": ["s3:GetObject", "s3:ListBucket"],
+      "Resource": [
+        "${aws_s3_bucket.gold.arn}",
+        "${aws_s3_bucket.gold.arn}/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": ["s3:PutObject"],
+      "Resource": "${aws_s3_bucket.athena_results.arn}/*"
+    }
+  ]
+}
+EOF
+}
+resource "aws_glue_crawler" "athena_crawler" {
+  name          = "athena-s3-crawler"
+  database_name = aws_glue_catalog_database.athena_db.name
+  role          = aws_iam_role.athena_role.arn
+
+  s3_target {
+    path = "s3://${aws_s3_bucket.gold.bucket}/"
+  }
+
+}
+resource "aws_iam_role_policy_attachment" "athena_attach" {
+  role       = aws_iam_role.athena_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
+
+resource "aws_iam_role_policy_attachment" "athena_attach_S3" {
+  role       = aws_iam_role.athena_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-  role       = aws_iam_role.lambda_etl_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+resource "aws_iam_role_policy_attachment" "athena_attach_crawler" {
+  role       = aws_iam_role.athena_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonAthenaFullAccess"
+}
+resource "aws_glue_catalog_database" "athena_db" {
+  name = "athena_database"
 }
 
-# Create Lambda function
-resource "aws_lambda_function" "etl_lambda" {
-  function_name    = "etl_lambda"
-  runtime         = "python3.9"  # Change runtime if needed
-  role            = aws_iam_role.lambda_etl_role.arn
-  handler         = "hello.lambda_handler"
-  timeout         = 30
+resource "aws_athena_workgroup" "athena_wg" {
+  name = "athena_curated_query"
+  force_destroy = true
 
-  # Path to the ZIP package of the Lambda function
-  filename         = "lambda_function.zip"
-  source_code_hash = filebase64sha256("lambda_function.zip")
-  environment  {
-    variables = {
-
-      DEST_BUCKET = aws_s3_bucket.silver.bucket
+  configuration {
+    result_configuration {
+      output_location = "s3://${aws_s3_bucket.athena_results.bucket}/"
     }
   }
-  tags = {
-    Name = "ETL Lambda Function"
-  }
 }
 
-# Optional: Add S3 trigger for Lambda
-resource "aws_lambda_permission" "s3_trigger" {
-  statement_id  = "AllowS3Invoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.etl_lambda.function_name
-  principal     = "s3.amazonaws.com"
-  source_arn    = aws_s3_bucket.bronze.arn
-}
-
-resource "aws_s3_bucket_notification" "s3_lambda_trigger" {
-  bucket = aws_s3_bucket.bronze.id
-
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.etl_lambda.arn
-    events             = ["s3:ObjectCreated:*"]
-  }
-
-  depends_on = [aws_lambda_permission.s3_trigger]
-}
 
 
